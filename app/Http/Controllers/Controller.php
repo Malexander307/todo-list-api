4110962a77d5ac67f08dead3b1691a89422643dc
Oleksandr <malex307ander@gmail.com>
2023-09-14 18:44:13 +0300

    made templates for responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function successResponse(mixed $data, string $message = 'success',int $status = 200): Response
+    {
+        return response([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    public function errorResponse(mixed $data, string $message = 'success',int $status = 500): Response
+    {
+        return response([
+            'status' => $status,
+            'message' => $message,
+        ]);
+    }
 }
